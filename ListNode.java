9757254aae5a07b5d05a93341d2671eb920b04bb
constWang <118581206+DayDayupWang@users.noreply.github.com>
2022-12-11 20:34:04 +0800

    Add files via upload

--- ListNode.java
+++ ListNode.java
@@ -6,11 +6,11 @@ public class ListNode {
     ListNode() {
     }
 
-    ListNode(int val) {
+    ListNode(int val) {// 新建一个链表，节点为对应数值
         this.val = val;
     }
 
-    ListNode(int val, ListNode next) {
+    ListNode(int val, ListNode next) {// 嵌套的方法连接链表
         this.val = val;
         this.next = next;
     }
--- solution203.java
+++ solution203.java
@@ -4,9 +4,9 @@ import org.xml.sax.ext.Locator2;
 
 public class solution203 {
     public static void main(String[] args) {
-        ListNode l1 = new ListNode(3);
+        ListNode l1 = new ListNode(6);
         l1.next = new ListNode(6);
-        l1.next.next = new ListNode(6);
+        l1.next.next = new ListNode(7);
         // while (l1!= null) {
         // System.out.println(l1.val);
         // l1 = l1.next;
@@ -28,24 +28,34 @@ public class solution203 {
         ListNode newHead = new ListNode();
         ListNode res = newHead;// 用于返回的头节点，从0开始
 
-        while (head != null) {
-            if (head.val == val) {
-                head = head.next;// 原链表取下个元素
-                continue;// 假如取到了要跳过的就continue
-            }
-            newHead.next = new ListNode(head.val);
-            newHead = newHead.next;
-            head = head.next;
-        }
+        // while (head != null) {
+        // if (head.val == val) {
+        // head = head.next;// 原链表取下个元素
+        // continue;// 假如取到了要跳过的就continue
+        // }
+        // newHead.next = new ListNode(head.val);
+        // newHead = newHead.next;
+        // head = head.next;
+        // }
+        Recursion(newHead, head, val);
+
         return res.next;
     }
 
-    public ListNode Recursion(ListNode l1, ListNode l2) {
+    public static void Recursion(ListNode l1, ListNode l2, int val) {
         if (l2 == null) {
-            return l1;
+            return;
         }
-        if (l2.val==val) {
-            
+        if (l2.val == val) {
+            l2 = l2.next;
+            Recursion(l1, l2, val);
+        } else {
+
+            l1.next = new ListNode(l2.val);
+            l1 = l1.next;
+            l2 = l2.next;
+            Recursion(l1, l2, val);
         }
+
     }
 }
